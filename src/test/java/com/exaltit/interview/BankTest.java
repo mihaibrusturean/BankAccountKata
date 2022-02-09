7c84efacf3bcbbcfc62e3561f63f800e547e6992
mbrusturean <mihai.brusturean@yahoo.com>
2022-02-09 20:34:52 +0100

    User story 2 - added tests for withdrawal functionality

--- src/test/java/com/exaltit/interview/BankTest.java
+++ src/test/java/com/exaltit/interview/BankTest.java
@@ -7,6 +7,7 @@ import static junit.framework.TestCase.assertEquals;
 
 public class BankTest {
 
+
     @Test
     public void testMakeDepositPositive() {
         Bank bank = new Bank();
@@ -30,47 +31,6 @@ public class BankTest {
         assertEquals(bankAccount.getOperations().size(), 1);
         Operation operation = bankAccount.getOperations().get(0);
         assertEquals(operation.getOperationType(), OperationType.DEPOSIT);
-        assertEquals(operation.getOperationState(), OperationState.DENIED);
-    }
-
-    @Test
-    public void testMakeWithdrawalFromEmpty() {
-        Bank bank = new Bank();
-        Client client = new Client("Client1");
-        BankAccount bankAccount = new BankAccount(client);
-        bank.makeWithdrawal(bankAccount, 100.0);
-        assertEquals(bankAccount.getBalance(), 0.0);
-        assertEquals(bankAccount.getOperations().size(), 1);
-        Operation operation = bankAccount.getOperations().get(0);
-        assertEquals(operation.getOperationType(), OperationType.WITHDRAWAL);
-        assertEquals(operation.getOperationState(), OperationState.DENIED);
-    }
-
-    @Test
-    public void testMakeWithdrawalAfterDeposit() {
-        Bank bank = new Bank();
-        Client client = new Client("Client1");
-        BankAccount bankAccount = new BankAccount(client);
-        bank.makeDeposit(bankAccount, 100.0);
-        assertEquals(bankAccount.getBalance(), 100.0);
-        bank.makeWithdrawal(bankAccount, 100.0);
-        assertEquals(bankAccount.getBalance(), 0.0);
-        assertEquals(bankAccount.getOperations().size(), 1);
-        Operation operation = bankAccount.getOperations().get(0);
-        assertEquals(operation.getOperationType(), OperationType.WITHDRAWAL);
-        assertEquals(operation.getOperationState(), OperationState.APPROVED);
-    }
-
-    @Test
-    public void testMakeWithdrawalNegative() {
-        Bank bank = new Bank();
-        Client client = new Client("Client1");
-        BankAccount bankAccount = new BankAccount(client);
-        bank.makeWithdrawal(bankAccount, -100.0);
-        assertEquals(bankAccount.getBalance(), 0.0);
-        assertEquals(bankAccount.getOperations().size(), 1);
-        Operation operation = bankAccount.getOperations().get(0);
-        assertEquals(operation.getOperationType(), OperationType.WITHDRAWAL);
         assertEquals(operation.getOperationState(), OperationState.DENIED);
     }
 }
