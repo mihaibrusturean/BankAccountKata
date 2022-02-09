7dd140d0d448352e5d04c2b6360066e0d04dff06
mbrusturean <mihai.brusturean@yahoo.com>
2022-02-09 20:40:59 +0100

    Revert "User story 2"
    This reverts commit 7c84efacf3bcbbcfc62e3561f63f800e547e6992.

--- src/main/java/com/exaltit/interview/Bank.java
+++ src/main/java/com/exaltit/interview/Bank.java
@@ -5,6 +5,7 @@ import com.exaltit.interview.model.Operation;
 import com.exaltit.interview.model.OperationType;
 
 public class Bank {
+
     public void makeDeposit(BankAccount bankAccount, double amount) {
         Operation operation = new Operation(bankAccount.getClient(), OperationType.DEPOSIT, bankAccount.getBalance(), amount);
         if (operation.isOperationValid()) {
@@ -15,5 +16,9 @@ public class Bank {
             operation.markAsDenied();
             bankAccount.addOperation(operation);
         }
+    }
+
+    public void makeWithdrawal(BankAccount bankAccount, double amount) {
+
     }
 }
